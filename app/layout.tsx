c774614869aa3875e7993e072fdcd6650d450ed6
Nabin <aarelaa.com>
2024-02-14 21:12:25 +0545

    initialize aos at root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,27 @@
 import type { Metadata } from "next";
-import { Inter, Architects_Daughter } from 'next/font/google'
+import { Inter, Architects_Daughter } from "next/font/google";
 import "./additionalCSS/globals.css";
 import Header from "@/components/molecules/header";
 import Footer from "@/components/molecules/footer";
+import { AOSInit } from "@/utils/aosUtil";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap'
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
 
 const architects_daughter = Architects_Daughter({
-  subsets: ['latin'],
-  variable: '--font-architects-daughter',
-  weight: '400',
-  display: 'swap'
-})
+  subsets: ["latin"],
+  variable: "--font-architects-daughter",
+  weight: "400",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Nepali Language Processing",
-  description: "Nepali Language Processing website tha provides Spelling Correction and Text Generation features.",
+  description:
+    "Nepali Language Processing website tha provides Spelling Correction and Text Generation features.",
 };
 
 export default function RootLayout({
@@ -29,13 +31,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
-      <div className="flex flex-col min-h-screen overflow-hidden">
-        <Header/>
-        {children}
-        <Footer />
-      </div>
-    </body>
-  </html>
+      <AOSInit />
+      <body
+        className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}
+      >
+        <div className="flex flex-col min-h-screen overflow-hidden">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
