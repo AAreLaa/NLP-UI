ba38b6639ac3e22bd3f5bdc9d4d233125ea87294
Nabin <aarelaa.com>
2024-02-19 13:28:43 +0545

    useState controlled from outside

--- components/atoms/textbox/index.tsx
+++ components/atoms/textbox/index.tsx
@@ -4,16 +4,17 @@ import React from "react";
 
 interface TextBoxProps {
   label: string;
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
 }
 export default function TextBox(props: Readonly<TextBoxProps>) {
-  const { label } = props;
-  const [text, setText] = React.useState("");
+  const { label, text, setText } = props;
   const changedvalue = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value);
   };
 
   const handleKey = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === " " || e.key === "Enter") {
+    if (e.key === " " || e.key === "Enter" && text.trim().length !== 0 ) {
       const lang = "ne";
       const url = `https://www.google.com/inputtools/request?ime=transliteration_en_${lang}&num=5&cp=0&cs=0&ie=utf-8&oe=utf-8&app=jsapi&text=${text}`;
       fetch(url)
